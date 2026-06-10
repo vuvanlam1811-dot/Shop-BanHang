08e6eb39d372e7efecd3a018d9bbba4cfac27aea
vuvanlam1811-dot <vuvanlam1811@gmail.com>
2026-06-10 13:49:31 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from appmaneger import AppManager
-
-def main():
-    print("--- Ứng dụng Quản lý Shop 4STeam đang khởi động ---")
-    app = AppManager()
-    app.run()
-
-if __name__ == "__main__":
-    main()
